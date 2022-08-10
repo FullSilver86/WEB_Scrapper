90919cef03729d13dc3c67df1d721045b87b6eee
pnowak <przemyslaw.nowak86@gmail.com>
2022-08-10 09:56:44 +0200

    removal of wrongly used class atributes

--- Classes/Classes.py
+++ Classes/Classes.py
@@ -2,40 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-
 class OLX_listing:
-    URL = ""
-    # lines are required for getting number of pages and correct offer links [used by multiple methods]
-    lines = []
-    search_object = ''
-    number_of_pages = 1
-    offer_links = []
-
 
     def __init__(self, URL, search_object):
         self.URL = urljoin(URL, search_object)
         self.search_object = search_object
 
-        OLX_listing.URL = self.URL
+        self.URL = self.URL
         page = requests.get(self.URL)
         soup = BeautifulSoup(page.content, "html.parser")
         results = soup.find_all('a')
         lines = [str(result.get('href')) for result in results]
-        OLX_listing.lines = lines
-        OLX_listing.search_object = search_object
-        OLX_listing.number_of_pages = int(self.get_page_number())
-        if OLX_listing.number_of_pages > 1:
-            for page_number in range(2, int(OLX_listing.number_of_pages)+1):
-                OLX_listing.lines.extend(self.get_all_pages_offer(f'https://www.olx.pl/oferty/q-{search_object}/?page={page_number}'))
-        self.get_links()
-        print(self.URL)
+        self.lines = lines
+        self.search_object = search_object
+        self.number_of_pages = int(self.get_page_number())
+        if self.number_of_pages > 1:
+            for page_number in range(2, int(self.number_of_pages)+1):
+                self.lines.extend(self.get_all_pages_offer(f'https://www.olx.pl/oferty/q-{search_object}/?page={page_number}'))
+        self.offer_links = self.get_links()
 
 
     def get_page_number(self):
         number_of_pages = 1
-        pages = [page for page in OLX_listing.lines if page.startswith(f"https://www.olx.pl/oferty/{OLX_listing.search_object}/?page")]
+        pages = [page for page in self.lines if page.startswith(f"https://www.olx.pl/oferty/{self.search_object}/?page")]
         if pages:
-            number_of_pages = pages[-2].lstrip(f'https://www.olx.pl/oferty/q-{OLX_listing.search_object}/?page=')
+            number_of_pages = pages[-2].lstrip(f'https://www.olx.pl/oferty/q-{self.search_object}/?page=')
         return number_of_pages
 
 
@@ -49,4 +40,5 @@ class OLX_listing:
 
 
     def get_links(self):
-        OLX_listing.offer_links =  [offer for offer in OLX_listing.lines if str(offer).startswith('https://www.olx.pl/d/oferta/')]
+        offer_links =  [offer for offer in self.lines if str(offer).startswith('https://www.olx.pl/d/oferta/')]
+        return offer_links
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from time import sleep
-
+from telesign.messaging import MessagingClient
 from Classes.Classes import OLX_listing
 
 #search object q-"name-2ndpartname"
-search_object = "q-gry-planszowe"
+search_object = "q-wing-foil"
 URL = "https://www.olx.pl/oferty/"
 
 '''
@@ -11,7 +11,7 @@ Get current offer for products
 '''
 
 current_offer = OLX_listing(URL, search_object)
-
+print(current_offer.URL)
 '''
 Checking for updates
 '''
@@ -19,7 +19,11 @@ Checking for updates
 while True:
     new_offer = OLX_listing(URL, search_object)
     new_offers = [new_link for new_link in new_offer.offer_links if new_link not in current_offer.offer_links]
-    print(current_offer.offer_links)
-    print(new_offer.offer_links)
+    print(len(new_offer.offer_links))
+    if new_offers:
+        current_offer.offer_links = new_offer.offer_links
     print(f" New listings : {new_offers}")
     sleep(600)
+
+
+
