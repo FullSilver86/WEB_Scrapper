664896e3a61550d5805963ccbbc26b0f6d20558d
pnowak <przemyslaw.nowak86@gmail.com>
2022-08-10 11:28:24 +0200

    Fixing problems with multiple pages - new logic

--- Classes/Classes.py
+++ Classes/Classes.py
@@ -8,14 +8,10 @@ class OLX_listing:
         self.URL = urljoin(URL, search_object)
         self.search_object = search_object
 
-        page = requests.get(self.URL)
+        page = requests.get(f"{self.URL}/?search%5Border%5D=created_at:desc")
         soup = BeautifulSoup(page.content, "html.parser")
         results = soup.find_all('a')
         self.lines = [str(result.get('href')) for result in results]
-        self.number_of_pages = int(self.get_page_number())
-        if self.number_of_pages > 1:
-            for page_number in range(2, int(self.number_of_pages)+1):
-                self.lines.extend(self.get_all_pages_offer(f'https://www.olx.pl/oferty/q-{search_object}/?page={page_number}'))
         self.offer_links = self.get_links()
 
 
@@ -27,15 +23,15 @@ class OLX_listing:
         return number_of_pages
 
 
-    def get_all_pages_offer(self, pageURL):
-        self.pageURL = pageURL
-
-        page = requests.get(self.pageURL)
-        soup = BeautifulSoup(page.content, "html.parser")
-        results = soup.find_all('a')
-        lines = [str(result.get('href')) for result in results]
-        return lines
-
+    def get_all_pages_offer(self):
+        for page_number in range(2, 25):
+            self.pageURL = f'https://www.olx.pl/oferty/q-{self.search_object}/?page={page_number}'
+            page = requests.get(self.pageURL)
+            soup = BeautifulSoup(page.content, "html.parser")
+            results = soup.find_all('a')
+            lines = [str(result.get('href')) for result in results]
+            self.lines.extend(lines)
+        self.offer_links = self.get_links()
 
     def get_links(self):
         offer_links =  [offer for offer in self.lines if str(offer).startswith('https://www.olx.pl/d/oferta/')]
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from time import sleep
 from Classes.Classes import OLX_listing
 
 #search object q-"name-2ndpartname"
-search_object = "q-wing-foil"
+search_object = "q-gry-planszowe"
 URL = "https://www.olx.pl/oferty/"
 
 '''
@@ -11,6 +11,7 @@ Get current offer for products
 '''
 
 current_offer = OLX_listing(URL, search_object)
+current_offer.get_all_pages_offer()
 print(current_offer.URL)
 '''
 Checking for updates
@@ -18,10 +19,11 @@ Checking for updates
 
 while True:
     new_offer = OLX_listing(URL, search_object)
-    new_offers = [new_link for new_link in new_offer.offer_links if new_link not in current_offer.offer_links]
+    new_offers = [new_link for new_link in set(new_offer.offer_links) if new_link not in current_offer.offer_links and "promoted" not in new_link]
     print(len(new_offer.offer_links))
+    print(new_offer.offer_links)
     if new_offers:
-        current_offer.offer_links = new_offer.offer_links
+        current_offer.offer_links.extend(new_offers)
     print(f" New listings : {new_offers}")
     sleep(600)
 
