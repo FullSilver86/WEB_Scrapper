3fac20fc93c38c727466d8f02f10f55773c8443f
pnowak <przemyslaw.nowak86@gmail.com>
2022-10-13 15:15:27 +0200

    Flask beginnigns

--- Classes/Classes.py
+++ Classes/Classes.py
@@ -1,10 +1,10 @@
 import requests
+from time import sleep
 
 class OLX_listing:
 
 
-
-    def __init__(self, URL, search_object):
+    def __init__(self, search_object, URL = 'https://www.olx.pl/api/v1/offers/?offset=0&limit=40&'):
         self.URL = URL
         self.search_object = search_object
         self.links = []
@@ -18,13 +18,42 @@ class OLX_listing:
         '''%22%3Atrue%2C%22fetchUrl%22%3Atrue%2C%22limit%22%3A20%7D%5D&sl=180b26a0c03x6be1b302''', headers=headers)
 
         api_response = list(page.text.split())
-        x = [url for url in api_response if 'https://www.olx.pl/d/oferta/' in url]
-        for y in x:
-            link = self.substring_after(y, "url").split('''"''')[2]
+        listing = [url for url in api_response if 'https://www.olx.pl/d/oferta/' in url]
+        for line in listing:
+            link = self.substring_after(line, "url").split('''"''')[2]
             self.links.extend([link])
 
 
 
-    def substring_after(self, x, delim):
-            return x.partition(delim)[2]
+    def substring_after(self, line, delim):
+            return line.partition(delim)[2]
 
+
+class Session_OLX:
+
+
+    def __init__(self, search_object):
+        self.search_object = search_object
+        self.listing = self.main()
+
+
+    def main(self):
+        '''
+          Get current offer for products
+          '''
+        current_offer = OLX_listing(self.search_object)
+
+        '''
+        Checking for updates
+        '''
+
+        # while True:
+        new_offer = OLX_listing(self.search_object)
+        new_offers = [new_link for new_link in set(new_offer.links) if new_link not in current_offer.links]
+        print(len(new_offer.links))
+        print(new_offer.links)
+        if new_offers:
+            current_offer.links.extend(new_offers)
+        print(f" New listings : {new_offers}")
+        return new_offer.links
+            # sleep(500)
---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, url_for, flash, redirect, session
+from flask_session import Session
+from Classes.Classes import Session_OLX, OLX_listing
+import os
+
+app=Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static'
+app.config['SECRET_KEY'] = 'your secret key'
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route('/', methods=["GET", "POST"])
+def search():
+    if not session.get("name"):
+        return redirect("/login")
+    else:
+        if request.method == 'POST':
+            product = request.form['product']
+
+            if not product:
+                flash('Product is required !')
+            else:
+                return redirect(url_for('product_list',product=product))
+
+
+    return render_template("search.html")
+
+@app.route('/product_list/<product>', methods=["GET"])
+def product_list(product):
+    new_search = Session_OLX(product)
+    listing = new_search.listing
+    return render_template("product_list.html", product=product, listing=listing)
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    # if form is submited
+    if request.method == "POST":
+        # record the user name
+        session["name"] = request.form.get("name")
+        # redirect to the main page
+        return redirect("/")
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session["name"] = None
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
